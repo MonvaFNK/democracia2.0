25264d5ed934e232e4a459f43fd05983c92d5fbe
Santiago Diaz Monvale <santiago.diaz.monvale@gmail.com>
2022-08-05 16:46:11 -0300

    agregando grafico 2.0

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,42 +1,28 @@
 let dog = "black";
 console.log(dog);
+google.charts.load('current',{packages:['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
 
-const data = {
-    'Ley1': 300000,
-    'Ley2': 195514,
-    'Ley3': 80460,
-};
-
-const ctx = document.getElementById('votaciones').getContext('2d');
-
-const myChart = new Chart(ctx, {
-	type: 'bar',
-	data: {
-		labels: Object.keys(data),
-		datasets: [
-			{
-				label: 'Votaciones Totales',
-				data: Object.values(data),
-			},
-		],
-        
-	},
-    options:{
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.5)',  // Bar 1
-            'rgba(54, 162, 235, 0.5)',  // Bar 2
-            'rgba(255, 206, 86, 0.5)',  // Bar 3
-        ],
-        borderWidth: 2,
-        borderColor: 'black'
-    },
-});
-
-const values = Object.values(data);
-
-const sum = values.reduce((accumulator, value) => {
-    return accumulator + value;
-}, 0);
-
-document.getElementById("total").innerHTML = "El total de votos registrados es "+sum;
-/* document.getElementById(total).innerHTML = sum; */
+    var data = google.visualization.arrayToDataTable([
+        ['Ley', 'Votos'],
+        ['Ley 1', 39000],
+        ['Ley 2', 45000],
+        ['Ley 3', 250000],
+        ['Ley 4', 658000],
+        ['Ley 5', 25000]
+    ]);
+    
+    var options = {
+        title: 'Leyes activas a votacion',
+        animation:{
+            duration: 1000,
+            easing: 'out',
+        },
+        backgroundColor: 'gray',
+    };
+    
+    var chart = new google.visualization.ColumnChart(document.getElementById('votaciones'));
+    chart.draw(data, options);
+    
+    }
